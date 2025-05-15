ccbbd9e19d71740d136883ee3d9ab60947721308
Kriz <k.jasinski.79@gmail.com>
2025-05-15 14:23:48 +0200

    Active list only

---
+++ cypress/e2e/activeToDoList.cy.js
@@ -0,0 +1,31 @@
+import pageData from '../fixtures/inputData'
+import urls from '../fixtures/urls'
+import { mainPageSelectors } from '../support/addToDo'
+import { completeSelectors } from '../support/completeToDo'
+import { activeSelectors } from '../support/activeToDoList'
+
+describe('Mark item as completed', () => {
+  it('Add 3 elements', () => {
+    cy.visit(urls.urlToDo);
+    cy.get(mainPageSelectors.searchField).should('be.visible');
+    cy.get(mainPageSelectors.searchField).click().type(pageData.inputData.firstElement).type('{enter}');
+    cy.get(mainPageSelectors.elementDisplay).should('contain',pageData.inputData.firstElement)
+    cy.get(mainPageSelectors.searchField).click().type(pageData.inputData.secondElement).type('{enter}');
+    cy.get(mainPageSelectors.elementDisplay).should('contain',pageData.inputData.secondElement)
+    cy.get(mainPageSelectors.searchField).click().type(pageData.inputData.thirdElement).type('{enter}');
+    cy.get(mainPageSelectors.elementDisplay).should('contain',pageData.inputData.thirdElement)
+
+    // Choose element and mark
+    cy.get(completeSelectors.checkboxToDo).eq(0).click()
+
+    // Check does element is marked
+    cy.get(completeSelectors.markedElement).eq(0).should('have.class', 'completed');
+
+    // Click 'Active' only list
+    cy.get(activeSelectors.activeButton).click()
+
+    // Check that displayed elements are only 'active'
+    cy.get(activeSelectors.activeElements).should('exist');
+    cy.get(activeSelectors.notActiveElements).should('not.exist');
+  })
+})
---
+++ cypress/support/activeToDoList.js
@@ -0,0 +1,5 @@
+export const activeSelectors = {
+    activeButton: 'a[href="#/active"]',
+    activeElements: 'ul > li[class=""]',
+    notActiveElements: 'ul > li[class="completed"]'
+}
